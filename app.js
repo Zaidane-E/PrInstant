68061bac874fc416850b10f82c2c9b6f0c9b973c
Zaidane-E <elhaouarizaidane@gmail.com>
2019-11-10 00:39:04 -0500

    Added: Jquery and js for smooth scrolling

--- app.js
+++ app.js
@@ -33,17 +33,15 @@ function display() {
 
 // == SMOOTH SCROLLING SECTION == //
 
-$(document).ready(function() { 
-
+$(document).ready(function() {
+  
   var scrollLink = $('.scroll');
-
-  scrollLink.click(function(event) {
-
+  
+  // Smooth scrolling
+  scrollLink.click(function(e) {
     e.preventDefault();
     $('body,html').animate({
       scrollTop: $(this.hash).offset().top
-    }, 1000 )
-
-  })
-
-  })
+    }, 1000 );
+  });
+})
--- index.md
+++ index.md
@@ -4,6 +4,7 @@
 
 <link rel="stylesheet" type="text/CSS" href="style.css">
 <script src="app.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 
